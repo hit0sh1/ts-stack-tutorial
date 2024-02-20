cf3690f349948c059e60786ec87feb866613b2cb
hit0sh1 <h.miyagawa365@gmail.com>
2024-02-20 22:05:00 +0900

    ✨ Navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,8 @@
 import Navigation from "@/components/auth/Navigation";
+import AuthProvider from "@/components/providers/AuthProvider";
+import TrpcProvider from "@/components/providers/TrpcProvider";
+import { getAuthSession } from "@/lib/nextauth";
+import ToastProvider from "@components/providers/ToastProvider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -15,28 +19,36 @@ interface RootLayoutProps {
 }
 
 const RootLayout = async ({ children }: RootLayoutProps) => {
+  // 認証情報取得
+  const user = await getAuthSession();
+
   return (
     <html lang="ja">
       <body className={inter.className}>
         <div className="flex min-h-screen flex-col">
-          <Navigation />
-          {children}
-          <main className="container mx-auto max-w-screen-md flex-1 px-2">{children}</main>
+          <AuthProvider>
+            <TrpcProvider>
+              <Navigation user={user} />
+              <ToastProvider />
+              {children}
+              <main className="container mx-auto max-w-screen-md flex-1 px-2">{children}</main>
 
-          {/* {フッター} */}
-          <footer className="py-5">
-            <div className="text-center text-sm">
-              Copyright @All rights reserved |{" "}
-              <a
-                href="https://www.youtube.com/@fullstackchannel"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="underline"
-              >
-                FullStack Channel
-              </a>
-            </div>
-          </footer>
+              {/* {フッター} */}
+              <footer className="py-5">
+                <div className="text-center text-sm">
+                  Copyright @All rights reserved |{" "}
+                  <a
+                    href="https://www.youtube.com/@fullstackchannel"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="underline"
+                  >
+                    FullStack Channel
+                  </a>
+                </div>
+              </footer>
+            </TrpcProvider>
+          </AuthProvider>
         </div>
       </body>
     </html>
---
+++ components/providers/ToastProvider.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { Toast } from "react-hot-toast";
+
+// トーストプロバイダー
+const ToastProvider = () => {
+  return <Toast />;
+};
+
+export default ToastProvider;
